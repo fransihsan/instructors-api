390bf09f877c33781ea783851ec914ad2e64930d
fransihsan <fransihsan8888@gmail.com>
2022-05-20 10:56:40 +0700

    add create and update instructor repository

--- repositories/instructor/instructor.go
+++ repositories/instructor/instructor.go
@@ -1,2 +1,35 @@
 package instructor
 
+import (
+	"errors"
+	I "instructors-api/entities/instructor"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type InstructorRepository struct {
+	db *gorm.DB
+}
+
+func NewInstructorRepository(db *gorm.DB) *InstructorRepository {
+	return &InstructorRepository{
+		db: db,
+	}
+}
+
+func (repo *InstructorRepository) Create(newInstructor I.Instructors) (I.Instructors, error) {
+	if err := repo.db.Create(&newInstructor).Error; err != nil {
+		log.Warn(err)
+		return I.Instructors{}, errors.New("gagal membuat instruktur baru")
+	}
+	return newInstructor, nil
+}
+
+func (repo *InstructorRepository) Update(instructorUpdate I.Instructors) (I.Instructors, error) {
+	if rowsAffected := repo.db.Model(&instructorUpdate).Updates(instructorUpdate).RowsAffected; rowsAffected == 0 {
+		return I.Instructors{}, errors.New("tidak ada perubahan pada data instructor")
+	}
+	repo.db.First(&instructorUpdate)
+	return instructorUpdate, nil
+}
