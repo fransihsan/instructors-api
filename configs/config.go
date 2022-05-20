e7ad247ca927dcf625bdb6837f34e67365a2dab9
fransihsan <fransihsan8888@gmail.com>
2022-05-20 09:38:45 +0700

    add struct configs

--- configs/config.go
+++ configs/config.go
@@ -1,2 +1,12 @@
 package configs
 
+type AppConfig struct {
+	PORT        int16
+	DB_DRIVER   string
+	DB_NAME     string
+	DB_PORT     int16
+	DB_HOST     string
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_LOC      string
+}
