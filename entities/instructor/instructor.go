f2646ab0254cdf92b3024c3b940a96d03713c2c2
fransihsan <fransihsan8888@gmail.com>
2022-05-20 09:59:10 +0700

    initial struct instructors

--- entities/instructor/instructor.go
+++ entities/instructor/instructor.go
@@ -1,2 +1,14 @@
 package instructor
 
+import (
+	"gorm.io/gorm"
+)
+
+type Instructors struct {
+	gorm.Model
+	Email                string `gorm:"type:varchar(255);not null;unique"`
+	Password             string `gorm:"type:varchar(255);not null"`
+	InstructorName       string `gorm:"type:varchar(255);not null"`
+	InstructorProfession string `gorm:"type:varchar(255);not null"`
+	AboutInstructor      string `gorm:"type:varchar(255)"`
+}
