a8a9466faf8f147dcf723606bfac84f34f6b2218
fransihsan <fransihsan8888@gmail.com>
2022-05-22 20:43:45 +0700

    add interface course repository

--- repositories/course/interface.go
+++ repositories/course/interface.go
@@ -1,2 +1,11 @@
 package course
 
+import C "instructors-api/entities/course"
+
+type Course interface {
+	Create(newInstructor C.Courses) (C.Courses, error)
+	Get() ([]FormatGetCourse, error)
+	GetDetail(ID uint) ([]FormatGetDetailCourse, error)
+	Update(userUpdate C.Courses) (C.Courses, error)
+	Delete(InstructorID, ID uint) error
+}
