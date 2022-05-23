46ea3468fc677c97a6ee46cd8229f0b9c1b10624
fransihsan <fransihsan8888@gmail.com>
2022-05-23 17:12:08 +0700

    add request controller course

---
+++ deliveries/controllers/course/request.go
@@ -0,0 +1,41 @@
+package course
+
+import (
+	C "instructors-api/entities/course"
+
+	"gorm.io/gorm"
+)
+
+type RequestCreate struct {
+	CourseTitle   string `json:"course_title" form:"course_title"`
+	CourseBenefit string `json:"course_benefit" form:"course_benefit"`
+	CoursePrice   uint   `json:"course_price" form:"course_price"`
+	CourseDesc    string `json:"course_desc" form:"course_desc"`
+}
+
+func (Req RequestCreate) ToEntityService(instructorID uint) C.Courses {
+	return C.Courses{
+		CourseTitle:   Req.CourseTitle,
+		CourseBenefit: Req.CourseBenefit,
+		CoursePrice:   Req.CoursePrice,
+		CourseDesc:    Req.CourseDesc,
+		IstructorID:   instructorID,
+	}
+}
+
+type RequestUpdate struct {
+	CourseTitle   string `json:"course_title" form:"course_title"`
+	CourseBenefit string `json:"course_benefit" form:"course_benefit"`
+	CoursePrice   uint   `json:"course_price" form:"course_price"`
+	CourseDesc    string `json:"course_desc" form:"course_desc"`
+}
+
+func (Req RequestUpdate) ToEntityService(ID uint) C.Courses {
+	return C.Courses{
+		Model:         gorm.Model{ID: ID},
+		CourseTitle:   Req.CourseTitle,
+		CourseBenefit: Req.CourseBenefit,
+		CoursePrice:   Req.CoursePrice,
+		CourseDesc:    Req.CourseDesc,
+	}
+}
